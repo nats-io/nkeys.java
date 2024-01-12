fc1d56e88639379776f80cd022c4a4ba23685f16
scottf <scottfauerbach@gmail.com>
2024-01-12 11:54:35 -0500

    Repackage to io.nats.client to match existing package in java client

--- src/main/java/io/nats/client/DecodedSeed.java
+++ src/main/java/io/nats/client/DecodedSeed.java
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package io.nats.client.impl;
+package io.nats.client;
 
 class DecodedSeed {
     public final int prefix;
